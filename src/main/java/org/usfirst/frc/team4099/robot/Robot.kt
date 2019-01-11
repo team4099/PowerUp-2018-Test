b67cdf76668aa66886fcb640e756ca4df58cc71e
ktorsh <ktorsh@DESKTOP-Q14N0SC.localdomain>
2019-01-11 16:09:21 -0500

    Add pneumatics test

--- src/main/java/org/usfirst/frc/team4099/robot/Robot.kt
+++ src/main/java/org/usfirst/frc/team4099/robot/Robot.kt
@@ -1,20 +1,19 @@
 package org.usfirst.frc.team4099.robot
 
-import com.ctre.phoenix.motorcontrol.ControlMode
+/*import com.ctre.phoenix.motorcontrol.ControlMode
 import com.ctre.phoenix.motorcontrol.FeedbackDevice
 import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced
-import com.ctre.phoenix.motorcontrol.can.TalonSRX
+import com.ctre.phoenix.motorcontrol.can.TalonSRX*/
 import edu.wpi.first.wpilibj.CameraServer
+import edu.wpi.first.wpilibj.DoubleSolenoid
 import edu.wpi.first.wpilibj.IterativeRobot
 import edu.wpi.first.wpilibj.Joystick
+import java.util.concurrent.TimeUnit
 
 
 class Robot : IterativeRobot() {
 
-    private val leftMasterSRX: TalonSRX = TalonSRX(8)
-    private val rightMasterSRX: TalonSRX = TalonSRX(1)
-    private val joystick = Joystick(0)
-    private var direction = -1
+    private val pneumaticPiston: DoubleSolenoid = DoubleSolenoid(1, 0)
 
     init {
         println("Working")
@@ -33,21 +32,19 @@ class Robot : IterativeRobot() {
     override fun teleopInit() {
 
 
+
     }
 
 
 
 
     override fun teleopPeriodic() {
-        if (joystick.getRawButton(2)) {
-            direction = 1
-        }
-        else {
-            direction = -1
-        }
 
-        leftMasterSRX.set(ControlMode.PercentOutput, 0.7 * direction)
-        rightMasterSRX.set(ControlMode.PercentOutput, 0.7 * direction)
+        pneumaticPiston.set(DoubleSolenoid.Value.kForward)
+        TimeUnit.SECONDS.sleep(5)
+        pneumaticPiston.set(DoubleSolenoid.Value.kReverse)
+        TimeUnit.SECONDS.sleep(5)
+
     }
 
     override fun testInit() {
@@ -61,8 +58,8 @@ class Robot : IterativeRobot() {
     }
 
     override fun autonomousPeriodic() {
-        leftMasterSRX.set(ControlMode.PercentOutput, 0.3)
-        rightMasterSRX.set(ControlMode.PercentOutput, 0.3)
+       // leftMasterSRX.set(ControlMode.PercentOutput, 0.3)
+        //rightMasterSRX.set(ControlMode.PercentOutput, 0.3)
     }
 
 
